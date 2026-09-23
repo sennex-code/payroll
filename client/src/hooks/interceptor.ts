97d5eb883d77e7fa5f66190960099fac6d7554cd
sennex <laincaputol21@gmail.com>
2026-09-23 14:36:32 +0800

    interceptor

--- client/src/hooks/interceptor.ts
+++ client/src/hooks/interceptor.ts
@@ -4,7 +4,7 @@ const getAccessToken = () => localStorage.getItem("wah_token") || "";
 const setAccessToken = (token: string) =>
   localStorage.setItem("wah_token", token);
 const axiosInterceptor = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 
   headers: {
