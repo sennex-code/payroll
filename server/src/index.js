ea509c9576e2ab623ab1e6fcfa464d4376b917fe
sennex <laincaputol21@gmail.com>
2026-09-23 15:08:12 +0800

    morecors

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ import hrReportsRoutes from "./routes/hrReportRoutes.js";
 import leaveRoutes from "./routes/leaveRoutes.ts";
 
 const app = express();
-const PORT = process.env.BACKEND_PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 // Updated CORS configuration to allow React and your custom headers
 
