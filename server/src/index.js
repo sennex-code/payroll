17bc73be43a6307e4979c1f6b081e4b96b30a9cc
sennex <laincaputol21@gmail.com>
2026-09-23 15:13:00 +0800

    morecors

--- server/src/index.js
+++ server/src/index.js
@@ -13,26 +13,31 @@ import leaveRoutes from "./routes/leaveRoutes.ts";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Updated CORS configuration to allow React and your custom headers
-
 app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "ngrok-skip-browser-warning",
+    ],
   }),
 );
+
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
 app.use("/api/file", fileRoutes);
 app.use("/api/hr-reports", hrReportsRoutes);
 app.use("/api/leaves", leaveRoutes);
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
